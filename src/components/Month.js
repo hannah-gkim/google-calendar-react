ef7c317e12d4d907c49f17a6582b4e23f53c6538
hannahk0108 <hannahk0108@gmail.com>
2021-11-30 15:38:24 -0600

    sidebar create button color changed

--- src/components/Month.js
+++ src/components/Month.js
@@ -4,7 +4,7 @@ import Day from "./Day";
 export default function Month({ month }) {
 
   return (
-    <div className="flex-1 grid grid-cols-7 grid-rows-5">
+    <div className="flex-1 grid grid-cols-7 grid-rows-5 ">
       {month.map((week, i) => (
         <React.Fragment key={i}>
           {week.map((day, idx) => (
