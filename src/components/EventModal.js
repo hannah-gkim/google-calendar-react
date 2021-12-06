3cc2d60b301eb61b7f093fbe23dc3788298d2cef
hannahk0108 <hannahk0108@gmail.com>
2021-12-06 00:40:59 -0600

    eventModal save button adding globalcontext state

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -1,8 +1,10 @@
 import React, { useContext, useState } from "react";
 import GlobalContext from "../context/GlobalContext";
-
+const labelsClasses = ["pink", "gray", "green", "blue", "red", "purple"];
 export default function EventModal() {
   const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectLabel, setSelectLabel] = useState(labelsClasses[0]);
   const { setShowEventModal, daySelected } = useContext(GlobalContext);
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
@@ -31,8 +33,46 @@ export default function EventModal() {
               schedule
             </span>
             <p>{daySelected.format("dddd, MMMM DD")}</p>
+            <span className="material-icons-outlined text-gray-400">
+              segment
+            </span>
+            <input
+              type="text"
+              name="description"
+              placeholder="Add a description"
+              value={description}
+              required
+              className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-red-200"
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <span className="material-icons-outlined text-gray-400">
+              bookmark_folder
+            </span>
+            <div className="flex gap-x-2">
+              {labelsClasses.map((labelClass, i) => (
+                <span
+                  key={i}
+                  onClick={() => setSelectLabel(labelClass)}
+                  className={`bg-${labelClass}-500 w-6 h-6 rounded-full flex items-center justify-center cursor-point`}
+                >
+                  {selectLabel === labelClass && (
+                    <span className="material-icons-outlined text-white text-sm">
+                      check
+                    </span>
+                  )}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
+        <footer className="flex justify-end border-t p-3 mt-5">
+          <button
+            type="submit"
+            className="bg-pink-500 hover:bg-pink-600 px-6 py-2 rounded text-white"
+          >
+            save
+          </button>
+        </footer>
       </form>
     </div>
   );
